2bf87a23cae84f049d4c342acc29ec1b0b05237a
ntfox <nt_fox@foxmail.com>
2019-02-28 18:15:50 +0800

    增加快速创建jdmsg

--- util/jsonMsg.go
+++ util/jsonMsg.go
@@ -1,9 +1,20 @@
 package util
 
+import (
+	"github.com/ntfox0001/svrLib/litjson"
+)
+
 // 创建一个json消息
 func JsMsg(msgId string) map[string]interface{} {
 	v := make(map[string]interface{})
 	v["msgId"] = msgId
 	v["errorId"] = "0"
 	return v
+}
+
+func JdMsg(msgId string) *litjson.JsonData {
+	jd := litjson.NewJsonData()
+	jd.SetKey("msgId", msgId)
+	jd.SetKey("errorId", "0")
+	return jd
 }
