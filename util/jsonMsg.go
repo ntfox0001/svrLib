518bde65415979c8bf451e43410b4defbae051ec
ntfox <nt_fox@foxmail.com>
2019-03-01 14:32:08 +0800

    增加msg继承keepdata

--- util/jsonMsg.go
+++ util/jsonMsg.go
@@ -4,6 +4,8 @@ import (
 	"github.com/ntfox0001/svrLib/litjson"
 )
 
+const MsgKeepDataName = "_MsgKeepDataName_"
+
 // 创建一个json消息
 func JsMsg(msgId string) map[string]interface{} {
 	v := make(map[string]interface{})
@@ -16,5 +18,14 @@ func JdMsg(msgId string) *litjson.JsonData {
 	jd := litjson.NewJsonData()
 	jd.SetKey("msgId", msgId)
 	jd.SetKey("errorId", "0")
+	return jd
+}
+
+func JdMsgFrom(msgId string, from *litjson.JsonData) *litjson.JsonData {
+	jd := JdMsg(msgId)
+	if from.HasKey(MsgKeepDataName) {
+		jd.SetKey(MsgKeepDataName, from.Get(MsgKeepDataName))
+	}
+
 	return jd
 }
